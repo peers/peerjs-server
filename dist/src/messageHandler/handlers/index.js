04ab2a078c345048000a05c32c20d3544d50a5d9
Jonas Gloning <34194370+jonasgloning@users.noreply.github.com>
2022-10-29 20:23:34 +0200

    chore(cleanup): "dist" should never have been committed to git

--- dist/src/messageHandler/handlers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransmissionHandler = exports.HeartbeatHandler = void 0;
-var heartbeat_1 = require("./heartbeat");
-Object.defineProperty(exports, "HeartbeatHandler", { enumerable: true, get: function () { return heartbeat_1.HeartbeatHandler; } });
-var transmission_1 = require("./transmission");
-Object.defineProperty(exports, "TransmissionHandler", { enumerable: true, get: function () { return transmission_1.TransmissionHandler; } });
