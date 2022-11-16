f600217051ab886fc797a8d60e6080f6e80fb433
Jonas Gloning <34194370+jonasgloning@users.noreply.github.com>
2022-11-16 21:49:17 +0100

    Revert "Merge branch '290-rangeerror-invalid-websocket-frame-rsv2-and-rsv3-must-be-clear' into rc"
    This reverts commit c10b21562a03f95850eecc715027eca0fa1caa37, reversing
    changes made to 3ff5873231c3319a22fee7ee97dacb8eb8c107e6.

--- src/services/webSocketServer/index.ts
+++ src/services/webSocketServer/index.ts
@@ -47,7 +47,6 @@ export class WebSocketServer extends EventEmitter implements IWebSocketServer {
   }
 
   private _onSocketConnection(socket: MyWebSocket, req: IncomingMessage): void {
-    socket.on("error", this._onSocketError);
     const { query = {} } = url.parse(req.url ?? '', true);
 
     const { id, token, key }: IAuthParams = query;
